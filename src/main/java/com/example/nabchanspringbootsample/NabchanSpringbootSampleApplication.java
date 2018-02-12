510e9929ac20cf2cd365855be2b0e523cc4cefa8
Taichi Uragami <backpaper0@gmail.com>
2018-02-12 10:35:18 +0900

    Initial commit

---
+++ src/main/java/com/example/nabchanspringbootsample/NabchanSpringbootSampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.nabchanspringbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NabchanSpringbootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NabchanSpringbootSampleApplication.class, args);
+	}
+}
