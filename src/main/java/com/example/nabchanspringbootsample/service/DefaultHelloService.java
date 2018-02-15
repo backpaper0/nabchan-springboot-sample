039c71fb9fda98b6a4db1dca6ba2a3d92d0c2406
kazuhira <kazuhira.m@gmail.com>
2018-02-15 19:38:02 +0900

    クラス名のtypo修正

--- src/main/java/com/example/nabchanspringbootsample/service/DefaultHelloService.java
+++ src/main/java/com/example/nabchanspringbootsample/service/DefaultHelloService.java
@@ -3,7 +3,7 @@ package com.example.nabchanspringbootsample.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class DfaultHelloService implements HelloService {
+public class DefaultHelloService implements HelloService {
 
     @Override
     public String getMessage(final String name) {
